21ab178e16f3f91139ebdea0a0b54a2051733285
name <raleksandrovic619@gmail.com>
2024-05-17 08:50:12 +0300

    Разработка бота

--- handlers/start.py
+++ handlers/start.py
@@ -1,30 +1,9 @@
-from aiogram import Router, F
-from aiogram.filters import Command
-from aiogram.types import BotCommand, Message, CallbackQuery
-from keyboards.start import *
-from main import bot
+from aiogram import Bot
+from aiogram.types import Message
 
-router = Router()
 
-@router.message(Command("start"))
-async def start_hander(msg: Message):
-
-    await bot.set_my_commands([
-        BotCommand(command='start', description='Запуск бота'),
-        BotCommand(command='anketa', description='Справка'),
-        BotCommand(command='get_achievement', description='Получить ачивку'),
-    ])
-
-    await msg.answer(text="Получение ачивок", reply_markup=kb_start_next)
-
-@router.callback_query(F.data =='next')
-async def next_handrer(callback_query: CallbackQuery):
-
-    await callback_query.message.edit_text(
-        'Страница 2', reply_markup=kb_start_back)
-
-@router.callback_query(F.data == 'back')
-async def back_handler(callback_query: CallbackQuery):
-    await callback_query.message.delete()
+async def get_start(message:Message, bot: Bot):
+    await bot.send_message(message.from_user.id, f"Привет, я Акуленок Джефф! Я готов к работе \n"
+                                               f"Бот помощник по ачивкам в учебных успехах студентов \n"
+                                               f"Вы сможете следить за списком своих ачивок \n\n\n")
     
-
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
-from handlers import include_routers
+from dotenv import load_dotenv
+import os
+from utils.commands import set_commands
+from handlers.start import get_start
+from aiogram.filters import Command
+load_dotenv('.env')
 
-bot =Bot(token="7088407944:AAEj6aTi2xMD1BlCan6k8UTSP3cRKFhv2Eo")
-db = Dispatcher()
+token = os.getenv('TOKEN')
+admin_id = os.getenv('ADMIN_ID')
+
+bot =Bot(token=token)
+dp =Dispatcher()
+
+async def start_bot(bot: Bot):
+    await bot.send_message(admin_id, text='Привет! Я Акуленок Джефф, я готов к работе.')
+
+    dp.startup.register(start_bot)
+
+dp.message.register(get_start, Command(commands='start'))
 
 
 async def main():
-    include_routers(db)
-    await db.start_polling(bot)
-    
+    await set_commands(bot)
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    finally:
+        await bot.session.close()
 
-
-if __name__=='___main___':
+if __name__ == '__main__':
     asyncio.run(main())
